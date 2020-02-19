316b8c373cba717b63d34f5cae9c5ad48eb93afe
Michael Oberwasserlechner <m.oberwasserlechner@byteowls.com>
2020-02-19 16:38:51 +0100

    test(): Add test skeleton to trigger gh actions

---
+++ src/test/java/com/byteowls/docker/boot/DockerSecretEnvPostProcessorTest.java
@@ -0,0 +1,24 @@
+package com.byteowls.docker.boot;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author m.oberwasserlechner@byteowls.com
+ */
+class DockerSecretEnvPostProcessorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void postProcessEnvironment() {
+    }
+
+    @Test
+    void getOrder() {
+    }
+}
