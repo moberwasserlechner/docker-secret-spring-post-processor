0c646bb51d93dc11edcf407cedf8938d2fb527bb
Michael Oberwasserlechner <m.oberwasserlechner@byteowls.com>
2020-02-19 20:13:46 +0100

    test(): order test

--- src/test/java/com/byteowls/docker/boot/DockerSecretEnvPostProcessorTest.java
+++ src/test/java/com/byteowls/docker/boot/DockerSecretEnvPostProcessorTest.java
@@ -2,6 +2,10 @@ package com.byteowls.docker.boot;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.SpringApplication;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,15 +14,24 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class DockerSecretEnvPostProcessorTest {
 
+    private DockerSecretEnvPostProcessor pp;
+//    @Mock
+//    private ConfigurableEnvironment env;
+//    @Mock
+//    private SpringApplication app;
+
     @BeforeEach
     void setUp() {
+        pp = new DockerSecretEnvPostProcessor();
     }
 
     @Test
     void postProcessEnvironment() {
+//        pp.postProcessEnvironment(env, app);
     }
 
     @Test
     void getOrder() {
+        assertEquals(Ordered.LOWEST_PRECEDENCE, pp.getOrder());
     }
 }
